989dbefbe17797cbe8e6d813048f0309fafa240a
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2026-03-26 09:50:23 +0200

    Update

--- Mediasovellus/operations/insertData.php
+++ Mediasovellus/operations/insertData.php
@@ -3,23 +3,28 @@ require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../db/dbConnect.php';
 global $DBH;
 
+if(!empty($_POST['title']) || !empty($_POST['description'])) {
 
-$sql = "INSERT INTO MediaItems (title, description, user_id, filename, filesize, media_type) VALUES (:title, :description, :user_id, :filename, :filesize, :media_type)";
+    $sql = "INSERT INTO MediaItems (title, description, user_id, filename, filesize, media_type) VALUES (:title, :description, :user_id, :filename, :filesize, :media_type)";
 
-$data = [
-    'title' => $_POST['title'],
-    'description' => $_POST['description'],
-    'user_id' => 1,
-    'filename' => 'tiedosto',
-    'filesize' => 1234,
-    'media_type' => 'image/jpeg',
-];
+    $data = [
+        'title' => $_POST['title'],
+        'description' => $_POST['description'],
+        'user_id' => 1,
+        'filename' => 'tiedosto',
+        'filesize' => 1234,
+        'media_type' => 'image/jpeg',
+    ];
 
-try {
-    $STH = $DBH->prepare($sql);
-    $STH->execute($data);
-    header('Location: ' . SITE_URL);
-} catch (PDOException $e) {
-    echo "Could not insert data into the database.";
-    file_put_contents('PDOErrors.txt', 'insertData.php - ' . $e->getMessage(), FILE_APPEND);
+    try {
+        $STH = $DBH->prepare($sql);
+        $STH->execute($data);
+        header('Location: ' . SITE_URL);
+    } catch (PDOException $e) {
+        echo "Could not insert data into the database.";
+        file_put_contents('PDOErrors.txt', 'insertData.php - ' . $e->getMessage(), FILE_APPEND);
+    }
+
+} else {
+    exit('No data or file');
 }
