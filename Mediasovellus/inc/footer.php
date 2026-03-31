c67cf6858f16c15d9397c6890e71f84a96b3346b
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2026-03-31 14:36:57 +0300

    Update

--- Mediasovellus/inc/footer.php
+++ Mediasovellus/inc/footer.php
@@ -1,3 +1,18 @@
 </div>
+
+<dialog id="notifications" <?php echo $_SESSION['notification'] ? 'open' : ''; ?>>
+    <h3>Note!!</h3>
+    <?php echo $_SESSION['notification']; ?>
+</dialog>
+<dialog id="errors" <?php echo $_SESSION['error'] ? 'open' : ''; ?>>
+    <h3>Error!!</h3>
+    <?php echo $_SESSION['error']; ?>
+</dialog>
+<?php
+
+$_SESSION['notification'] = '';
+$_SESSION['error'] = '';
+
+?>
 </body>
 </html>
--- Mediasovellus/index.php
+++ Mediasovellus/index.php
@@ -7,6 +7,9 @@ if (!isset($_SESSION['user'])) {
     header('Location: ' . SITE_URL . 'users.php');
     exit;
 }
+echo '<pre>';
+print_r($_SESSION['user']);
+echo '</pre>';
 
 require_once __DIR__ . '/inc/header.php';
 require_once __DIR__ . '/inc/etusivu.php';
