21b8bed62a0e465b6556e35727fd24e7b3a9df43
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2026-03-31 14:42:53 +0300

    Update

--- Mediasovellus/inc/media-table.php
+++ Mediasovellus/inc/media-table.php
@@ -30,7 +30,9 @@ global $DBH;
                 echo '<td>' . $row['created_at'] . '</td>';
                 echo '<td>' . $row['username'] . '</td>';
                 echo '<td><img src="' . SITE_URL . 'uploads/' . $row['filename'] . '"></td>';
-                echo '<td><a href="' . SITE_URL . '?operation=modify&media_id=' . $row['media_id'] . '">Modify</a> / Delete</td>';
+                echo '<td>';
+                echo '<a href="' . SITE_URL . '?operation=modify&media_id=' . $row['media_id'] . '">Modify</a></td>';
+                echo '<a href="' . SITE_URL . '?operation=delete&media_id=' . $row['media_id'] . '">Delete</a></td>';
                 echo '</tr>';
             }
         } catch (PDOException $error) {
